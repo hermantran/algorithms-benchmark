7cacf0a34016c6ec8a27aad66de692401adc7bb6
Herman <hermantr@usc.edu>
2014-01-02 23:50:35 -0800

    added random test case

--- spec/bubbleSort.spec.js
+++ spec/bubbleSort.spec.js
@@ -1,9 +1,5 @@
 describe('Bubble sort', function() {
   var sort = window.algorithms.bubbleSort;
   
-  for (var test in testCases) {
-    if (testCases.hasOwnProperty(test)) {
-      addTest(sort, test);
-    }
-  }
+  runTests(sort);
 });
--- spec/specHelper.js
+++ spec/specHelper.js
@@ -1,3 +1,4 @@
+var random = randomArray();
 var testCases = {
   'empty array': {
     actual: [],
@@ -22,6 +23,10 @@ var testCases = {
   'varied': {
     actual: [-922, 5, -21, 8177, -21, 7.7, 1.3, 0, -4, 67],
     expected: [-922, -21, -21, -4, 0, 1.3, 5, 7.7, 67, 8177]
+  },
+  'random': {
+    actual: random.unsorted,
+    expected: random.sorted
   }
 };
   
@@ -33,6 +38,31 @@ function addTest(sort, test) {
     sort(array);
     expect(array).toEqual(testCases[test].expected);
   });
+}
+
+function runTests(sort) {
+  for (var test in testCases) {
+    if (testCases.hasOwnProperty(test)) {
+      addTest(sort, test);
+    }
+  }
+}
+
+function randomArray() {
+  var random = [];
+  for (var i = 0; i < 100; i++) {
+      var el = Math.round(Math.random() * 100);
+      random.push(el);
+  }
+  
+  return {
+    unsorted: random,
+    sorted: JSON.parse(JSON.stringify(random)).sort(inOrder)
+  };
+}
+
+function inOrder(first, second) {
+  return first - second;
 }
 
 beforeEach(function() {
